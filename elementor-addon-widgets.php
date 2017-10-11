b8c2b7c2e639373a0a70f01916fb5851bf7f2344
Andrei Băicuș <baicusandrei@me.com>
2017-10-11 16:11:07 +0300

    Version Bump to 1.1.1

--- elementor-addon-widgets.php
+++ elementor-addon-widgets.php
@@ -3,7 +3,7 @@
  * Plugin Name: Elementor Addons & Widgets
  * Plugin URI: https://themeisle.com/
  * Description: Adds new Addons & Widgets that are specifically designed to be used in conjunction with the Elementor Page Builder.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: ThemeIsle
  * Author URI: https://themeisle.com/
  * Requires at least:   4.4
@@ -22,7 +22,7 @@ if ( ! defined( 'WPINC' ) ) {
 ------------------------------------------ */
 
 /* Set plugin version constant. */
-define( 'EA_VERSION', '1.1.0' );
+define( 'EA_VERSION', '1.1.1' );
 
 /* Set constant path to the plugin directory. */
 define( 'EA_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
