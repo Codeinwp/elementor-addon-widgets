cb075d7656ca91eccb4b64ad37620dc192aac97b
abaicus <baicusandrei@me.com>
2017-11-17 11:56:15 +0200

    Fixed JS error. Check for elementor object.

--- widgets/elementor/js/obfx-grid.js
+++ widgets/elementor/js/obfx-grid.js
@@ -14,24 +14,26 @@
 		}
 	);
 
-	$( window ).on(
-		'elementor/frontend/init', function () {
-			elementor.hooks.addAction(
-				'panel/open_editor/widget/obfx-posts-grid', function (panel) {
-					var $element = panel.$el.find( '.elementor-control-section_grid_image' );
-					$element.click(
-						function() {
-							panel.$el.find( '.elementor-control-grid_image_height .elementor-control-input-wrapper' ).mouseup(
-								function() {
-									checkImageSize();
-								}
-							);
-						}
-					);
-				}
-			);
-		}
-	);
+    if ( typeof elementor !== 'undefined' ) {
+        $(window).on(
+            'elementor/frontend/init', function () {
+                elementor.hooks.addAction(
+                    'panel/open_editor/widget/obfx-posts-grid', function (panel) {
+                        var $element = panel.$el.find('.elementor-control-section_grid_image');
+                        $element.click(
+                            function () {
+                                panel.$el.find('.elementor-control-grid_image_height .elementor-control-input-wrapper').mouseup(
+                                    function () {
+                                        checkImageSize();
+                                    }
+                                );
+                            }
+                        );
+                    }
+                );
+            }
+        );
+    }
 
 	/**
 	 * Check the container and image size.
